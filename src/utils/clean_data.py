a3b14b9b26d794164ecb5c5531899954b54ebf7d
Meliana Zerroug <meliana.zerroug@edu.esiee.fr>
2025-11-04 13:30:09 +0000

    Modif du fichier clean_data

--- src/utils/clean_data.py
+++ src/utils/clean_data.py
@@ -12,14 +12,12 @@ df = df.drop(columns=colonnes_drop, errors='ignore')
 #On standardise les noms de colonnes
 df.columns = df.columns.str.lower().str.replace(' ', '_')
 
-
 #On va ensuite nettoyer les données avec du texte 
 #On définit les colonnes textuelles à nettoyer
 text_columns = ["zone", "produit", "unité"]
 
 for col in text_columns:
     df[col] = df[col].astype(str).str.strip()  # supprimer espaces
-    df[col] = df[col].apply(lambda x: re.sub(r'[^\w\s]', ' ', x))  # supprimer caractères spéciaux on les remplace par rien
 
 #on cherche a extraire l'année au centre 
 def extract_middle_year(s): 
@@ -31,6 +29,6 @@ def extract_middle_year(s):
         return s
 
 df["année"] = df["année"].apply(extract_middle_year)
-df["valeur"] = pd.to_numeric(df["valeur"], errors='coerce')
-
+df["produit"] = df["produit"].str.strip()
+df["valeur"] = pd.to_numeric(df["valeur"].str.replace(',', '.') if df["valeur"].dtype == "object" else df["valeur"], errors='coerce')
 df.to_csv('data/cleaned/clean_data.csv', index=False)
