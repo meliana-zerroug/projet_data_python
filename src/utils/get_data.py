b27407d86a769e9bb2d7b1d1cbe646afce348f0a
Damien Ledey <damien.ledey@edu.esiee.fr>
2025-11-04 13:52:46 +0000

    Ajout du fichier get_data.py qui récupère le fichier csv depuis la base de données de Faostat avec son API

---
+++ src/utils/get_data.py
@@ -0,0 +1,43 @@
+# Script pour récupérer des données FAOSTAT via faostat.get_data_df
+
+import faostat
+
+# Codes du jeu de données FAOSTAT 
+dataset = 'FS'
+
+# Définition des paramètres
+# =======================
+
+# Liste des codes ou noms de pays (ISO3 ou noms FAO)
+pays = None  # None pour tous les pays
+
+# Liste des items (produits)
+items = [21004, 21001, 21042, 210420, 22013, 220001, 21061]
+
+# Choix du type de valeur (Valeur approchée : 6120, Estimation haute et basse : 6210)
+elements = [6120]
+
+# Liste des années sans prendre 2025 pour ne pas avoir de fausses données / mal insérées dans la base de données
+annees = [i*10+3 for i in range(2000, 2024)] 
+
+# Construction du dictionnaire de paramètres
+params = {
+    'area': pays,
+    'item': items,
+    'element': elements,
+    'year3': annees
+}
+
+# =======================
+
+# Récupération des données
+try:
+    df = faostat.get_data_df(dataset, pars=params)
+    print(f"Données téléchargées : {df.shape[0]} lignes x {df.shape[1]} colonnes")
+except Exception as e:
+    print("Erreur lors du téléchargement :", e)
+    
+# Sauvegarde locale
+output_file = 'data/raw/faostat_data.csv'
+df.to_csv(output_file, index=False)
+print(f"Données sauvegardées dans : {output_file}")
