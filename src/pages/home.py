ec5510ffa1ed4ac659b0a931e0f360db55f44af1
Meliana Zerroug <meliana.zerroug@edu.esiee.fr>
2025-11-05 09:10:13 +0000

    Ajout de la trend line dans home.py

--- src/pages/home.py
+++ src/pages/home.py
@@ -3,21 +3,14 @@ import plotly.express as px
 import pandas as pd
 from src.components.filter_component import filter_component
 from src.components.map_component import map_component
+from src.components.trend_line import trend_line_component
+
 
 # Initialisation de l'application Dash 
 app = Dash(__name__)
 
 # Load data
 df = pd.read_csv("data/cleaned/clean_data.csv")
-
-# Liste des indicateurs pertinents pour la malnutrition et l'obésité
-indicateurs_pertinents = [
-    "Prévalence de la sous-alimentation (%) (moyenne sur 3 ans)",
-    "Nombre d'adultes (18 ans ou plus) obèses (millions)",
-    "Nombre de personnes sous-alimentées (millions) (moyenne sur 3 ans)",
-    "Prévalence de l obésité chez l adulte  18 ans ou plus",
-    "Disponibilité alimentaire par habitant utilisée dans l'estimation de la prévalence de la sous-alimentation (kcal/personne/jour)"
-]
 
 from src.utils.country_mapping import country_mapping
 
@@ -27,9 +20,6 @@ mask_nutrition = df["valeur"].notna()
 # Appliquer le filtre et normaliser les noms des pays
 df = df[mask_nutrition].copy()
 df["zone"] = df["zone"].replace(country_mapping)
-
-# Préparation des données pour l'affichage
-sample_data = df.groupby("zone")["valeur"].mean().head()
 
 # Layout principal
 app.layout = html.Div(
@@ -47,11 +37,13 @@ app.layout = html.Div(
                 "position": "relative",
                 "width": "100%",
                 "height": "100vh",
-                "padding": "0"
+                "padding": "0",
+                "overflow": "auto" 
             },
             children=[
                 filter_component(df),
-                map_component()
+                map_component(),
+                trend_line_component(df)
             ]
         )
     ]
